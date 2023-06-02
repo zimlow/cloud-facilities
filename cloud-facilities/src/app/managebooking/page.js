c18fd11c6eef3ee63efa1de07f380be15c75a68c
zimlow <ziminglow@gmail.com>
2023-06-03 00:46:23 +0800

    changed page name
    theTeam > aboutUs
    myBooking > managebooking

---
+++ cloud-facilities/src/app/managebooking/page.js
@@ -0,0 +1,47 @@
+import { prisma } from "@/db";
+import { redirect } from "next/navigation";
+import { GET } from "@/app/api/mybooking/route.js";
+import { Children } from "react";
+
+async function getBooking(data) {
+  "use server";
+  const res = await prisma.bookings.findFirst({
+    where: {
+      AND: [
+        { booking_reference: data.get("booking_reference").valueOf() },
+        { lastName: data.get("lname").valueOf() },
+      ],
+    },
+    include: {
+      trip: true,
+    },
+  });
+  redirect(`/managebooking/overview?booking=${res.booking_reference}`);
+}
+
+const managebooking = () => {
+  return (
+    <>
+      <div className="text-center place-self-center text-9xl">Manage Booking</div>
+      <br />
+      <div className="justify-end w-full">
+        <form action={getBooking} className="w-full">
+          <input
+            placeholder="Enter your Ten-character Booking Reference"
+            name="booking_reference"
+            className="w-4/12"
+          ></input>
+          <br />
+          <input placeholder="Enter your last/family name" name="lname" className="w-4/12"></input>
+          <br />
+
+          <button type="submit" className="w-2/12 bg-blue-200">
+            Manage Booking
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default managebooking;
