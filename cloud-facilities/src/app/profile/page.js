e2c91fac40aaf2b3a00cd54f55711615c2c43838
zimlow <ziminglow@gmail.com>
2023-06-05 13:37:03 +0800

    protected profile page

--- cloud-facilities/src/app/profile/page.js
+++ cloud-facilities/src/app/profile/page.js
@@ -1,7 +1,55 @@
 import React from "react";
+import Footer from "../components/Footer";
+import { prisma } from "@/db";
 
-const page = () => {
-  return <div>hello</div>;
+//   only authenticated user can see this
+const ProfilePage = () => {
+  // const getUser = async () => {
+  //   const user = await prisma.user.findUnique({
+
+  //   })
+  // }
+
+  return (
+    <>
+      <main className="profile-page">
+        <section className="relative block" style={{ height: "500px" }}>
+          <div
+            className="absolute top-0 w-full h-full bg-center bg-cover"
+            style={{
+              backgroundImage:
+                "url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2710&q=80')",
+            }}
+          >
+            <span id="blackOverlay" className="w-full h-full absolute opacity-50 bg-black"></span>
+          </div>
+          <div
+            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
+            style={{ height: "70px" }}
+          >
+            <svg
+              className="absolute bottom-0 overflow-hidden"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            ></svg>
+          </div>
+        </section>
+        <section className="relative py-16 bg-gray-300">
+          <div className="container mx-auto px-4">
+            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
+              <div className="px-6">
+                <div className="mt-10 py-10 border-t border-gray-300 text-center"></div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
 
-export default page;
+export default ProfilePage;
